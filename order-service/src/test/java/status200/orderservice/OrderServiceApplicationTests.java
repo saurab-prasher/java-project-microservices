59a1a9befa32ea7ee14de8055cd098561f1fd922
parth0718 <112981952+parth0718@users.noreply.github.com>
2024-04-18 07:35:01 -0400

    OrderServiceApplicationTests.java

--- order-service/src/test/java/status200/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/status200/orderservice/OrderServiceApplicationTests.java
@@ -3,11 +3,15 @@ package status200.orderservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// Indicates that this class is a Spring Boot test class and will load the Spring application context
 @SpringBootTest
 class OrderServiceApplicationTests {
 
+	// This method is a test method which will be executed when running the test
 	@Test
 	void contextLoads() {
+		// Test to ensure that the Spring application context loads successfully
 	}
 
 }
+
