dbc862862051aac9676d6cb55ab2b2d91ecf9410
parth0718 <112981952+parth0718@users.noreply.github.com>
2024-04-18 07:27:30 -0400

    ImageServiceApplicationTests.java

--- image-service/src/test/java/status200/imageservice/ImageServiceApplicationTests.java
+++ image-service/src/test/java/status200/imageservice/ImageServiceApplicationTests.java
@@ -1,13 +1,19 @@
 package status200.imageservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootTest
-class ImageServiceApplicationTests {
+// Annotating this class with @SpringBootApplication to mark it as a Spring Boot application
+@SpringBootApplication
+// Annotating with @EnableDiscoveryClient to enable service discovery for this application
+@EnableDiscoveryClient
+public class ImageServiceApplication {
 
-    @Test
-    void contextLoads() {
+    // Main method, the entry point of the application
+    public static void main(String[] args) {
+        // Start the Spring Boot application
+        SpringApplication.run(ImageServiceApplication.class, args);
     }
 
 }
